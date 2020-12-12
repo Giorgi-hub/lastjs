2b86ad25beef2604e52b2b27eb8c8f84aa584db1
Giorgi-hub <74665134+Giorgi-hub@users.noreply.github.com>
2020-12-12 11:12:57 +0400

    main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+// // დავალება [1]
+// // მოცემულია მასივი:
+//  let array = [5, 10, 25, 100, 6, 18];
+// // map-ის გამოყენებით შექმენით ახალი მასივი, რომელშიც array მასივიდან თითოეული ელემენტი იქნება 2-ზე გაყოფილი;
+//  let newarray = array .map(func);
+//  let array2 = array.map(func);
+//  function func (num){
+//      return num * 0.5;
+//  }
+//  console.log(array2)
+// // დავალება [2]
+// // მოცემულია მასივი:
+//  let array = ["hello", "javascript", "html", 43, "css", "scss", 76, "bootstrap", 88, 59];
+// // filter-ის გამოყენებით გაფილთრეთ და გამოიტანეთ მარტო სტრინგები;
+//  let array = ["hello", "javascript", "html", 43, "css", "scss", 76, "bootstrap", 88, 59];
+//  var strings = array.filter (value => typeof value === 'string');
+//  console.log(strings);
+// // დავალება [3]
+// მოცემულია მასივი:
+// let array = ["hello", "javascript", "html", 43, "css", "scss", 76, "bootstrap", 88, 59];
+// // filter-ის გამოყენებით გაფილთრეთ და გამოიტანეთ მარტო ციფრები;
+// let array = ["hello", "javascript", "html", 43, "css", "scss", 76, "bootstrap", 88, 59];
+// var nums = array.filter(value => typeof value === 'number');
+// console.log(nums);
+// // დავალება [4]
+// // მოცემულ მასივში გვაქვს 5 ობიექტი; 
+// // გამოიტანეთ ის ქალაქი, რომლის მოსახლეობა 3 მილიონზე ნაკლებია - filterის გამოყენებით;
+// let cities = [
+//      {name: 'los angeles', population: 3792621},
+//      {name: 'new york', population: 8175133},
+//      {name: 'chicago', population: 2695598},
+//      {name: 'houston', population: 2099451},
+//      {name: 'philadelphia', population: 1526006}
+// ];
